b59105e5e369a6b7c1f84368ae17eb7ec7aa4f0b
Daniel Liebert <daniel.liebert@gmail.com>
2022-06-08 18:46:27 -0300

    Create engine and make calculator usable with buttons

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const multiply = (a,b) => a*b;
 
 const divide = (a,b) => a/b;
 
-const operator = (a, b, operator) => {
+const operate = (a, b, operator) => {
     return  operator === '+' ? add(a,b) :
             operator === '-' ? subtract(a,b) :
             operator === '*' ? multiply(a,b) :
@@ -14,27 +14,74 @@ const operator = (a, b, operator) => {
 };
 
 function updateDisplayValue (value) {
-    displayValue.textContent += value;
+    displayValue.textContent = value;
 }
 
 let displayValue = document.querySelector('#display');
+let number = '';
+let operator = '';
+let startNewNumber = true;
 
-let buttons = document.querySelectorAll('.operator');
-
-buttons.forEach(button => {
+document.querySelectorAll('button').forEach(button => {
+    if(button.classList.value.split(' ').some(value => value === 'number')) {
         button.addEventListener('click', () => {
-        updateDisplayValue(button.id)
-    })
-})
-
-// let a = 5;
-// let b = 2;
-// console.log(add(a,b));
-// console.log(subtract(a,b));
-// console.log(multiply(a,b));
-// console.log(divide(a,b));
-// console.log(operator(a, b, '+'));
-// console.log(operator(a, b, '-'));
-// console.log(operator(a, b, '*'));
-// console.log(operator(a, b, '/'));
-// console.log(operator(a, b, '='));
+            if (startNewNumber === true) {
+                displayValue.textContent = '';
+                startNewNumber = false;
+            };
+            updateDisplayValue(displayValue.textContent + button.id);
+        });
+    };
+    if(button.classList.value.split(' ').some(value => value === 'operator')) {
+        button.addEventListener('click', () => {
+            if (displayValue.textContent === 'Error: dividing by 0') {
+                operator = '';
+                startNewNumber = true;
+            } else if (operator === '') {
+                number = displayValue.textContent;
+                operator = button.id;
+                startNewNumber = true;
+            } else {
+                if (operator === '/' && displayValue.textContent === '0') {
+                    updateDisplayValue('Error: dividing by 0');
+                    number = '';
+                    operator = '';
+                    startNewNumber = true;
+                } else {
+                    updateDisplayValue(Math.round(operate(number*1, displayValue.textContent*1, operator)*100)/100);
+                    number = displayValue.textContent;
+                    operator = button.id;
+                    startNewNumber = true;
+                };
+            };
+        });
+    };
+    if(button.classList.value.split(' ').some(value => value === 'equal')) {
+        button.addEventListener('click', () => {
+            if(number !== '' && operator !== '') {
+                if (displayValue.textContent === 'Error: dividing by 0') {
+                    operator = '';
+                    startNewNumber = true;
+                } else if (operator === '/' && displayValue.textContent === '0') {
+                    updateDisplayValue(displayValue.textContent = 'Error: dividing by 0');
+                    number = '';
+                    operator = '';
+                    startNewNumber = true;
+                } else {
+                    updateDisplayValue(displayValue.textContent = Math.round(operate(number*1, displayValue.textContent*1, operator)*100)/100);
+                    number = '';
+                    operator = '';
+                    startNewNumber = true;
+                };
+            };
+        });
+    };
+    if(button.classList.value.split(' ').some(value => value === 'clear')) {
+        button.addEventListener('click', () => {
+            updateDisplayValue(displayValue.textContent = '0');
+            number = '';
+            operator = '';
+            startNewNumber = true;
+        });
+    };
+});
